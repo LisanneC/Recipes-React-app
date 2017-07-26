29375e78d8cc5dda0879c3063f976df26b00033e
lisanne <lisanne.chin@gmail.com>
2017-07-26 14:53:00 +0200

    Create and dispatch recipe action

--- src/reducers/recipes.js
+++ src/reducers/recipes.js
@@ -1,8 +1,11 @@
 // src/reducers/recipes.js
 import { SEED_RECIPES } from '../actions/recipes/seed'
 import { UPDATE_RECIPE } from '../actions/recipes/update'
+import { CREATE_RECIPE } from '../actions/recipes/create'
 
-export default (state = [], { type, payload } = {}) => {
+const initialState = []
+
+export default (state = initialState, { type, payload } = {}) => {
   switch (type) {
     case SEED_RECIPES :
       return [].concat(payload)
@@ -14,6 +17,9 @@ export default (state = [], { type, payload } = {}) => {
         if (recipe._id !== _id) return recipe
         return Object.assign({}, recipe, updates)
       })
+
+    case CREATE_RECIPE :
+      return [Object.assign({}, payload)].concat(state)
 
     default :
       return state
